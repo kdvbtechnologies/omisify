0afa31e0cd7f7b02dd105dac833d5829b1afbb17
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-22 15:01:26 -0200

    m

--- src/components/HomeAction.js
+++ src/components/HomeAction.js
@@ -2,11 +2,21 @@
 <Helmet>
         <body className="body-navbar"></body>
       </Helmet>
+
+      
+<footer>
+  <p>Author: Hege Refsnes<br>
+  <a href="mailto:hege@example.com">hege@example.com</a></p>
+</footer>
       */
 export default function HomeAction() {
   return (
     <>
       <h1>Bienvenue</h1>
+      <p>
+        Responsive Design Creation de logo Gratuit Certificat SSL Gratuit
+        Hebergement
+      </p>
 
       <footer>
         <p>© 2022 Omisify</p>
