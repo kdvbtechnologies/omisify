ff5c0509ef331e9a672d0330495f2d1820420b98
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-22 15:25:36 -0200

    m

--- src/components/HomeAction.js
+++ src/components/HomeAction.js
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 /*import { Helmet } from "react-helmet";
 <Helmet>
         <body className="body-navbar"></body>
@@ -9,17 +10,19 @@
   <a href="mailto:hege@example.com">hege@example.com</a></p>
 </footer>
       */
+
 export default function HomeAction() {
   return (
     <>
       <h1>Bienvenue</h1>
       <p>
         Responsive Design + bonus: Creation de logo, Certificat SSL, Hebergement
+        Creation de site internet au prix de 10 euros. Omisify est une
+        entreprise qui crée des sites internet au prix de 10 euros. Le paimenent
+        se fait après la livraison. Visitez leur site internet en cliquant ici
+        https://www.omisify.com
       </p>
-
-      <footer>
-        <p>© 2022 Omisify</p>
-      </footer>
+      <Footer />
     </>
   );
 }
