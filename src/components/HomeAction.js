51df4cb72d31af37191c2e18763f4cec232045cb
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-22 14:13:53 -0200

    m

--- src/components/HomeAction.js
+++ src/components/HomeAction.js
@@ -7,6 +7,10 @@ export default function HomeAction() {
         <body className="body-navbar"></body>
       </Helmet>
       <h1>Bienvenue</h1>
+      <p>
+        N.B : Les paiements à la livraison sont uniquement réservés aux nouveaux
+        sites.
+      </p>
       <footer>
         <p>© 2022 Omisify</p>
       </footer>
