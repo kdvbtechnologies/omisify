5b72f251188be5f8ca14f6ed8212df86fb71369f
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-22 15:41:49 -0200

    m

--- src/components/HomeAction.js
+++ src/components/HomeAction.js
@@ -17,15 +17,16 @@ export default function HomeAction() {
       <div className="cards">
         <div className="card">
           <h1>Bienvenue</h1>
+
+          <p>
+            Responsive Design + bonus: Creation de logo, Certificat SSL,
+            Hebergement Creation de site internet au prix de 10 euros. Omisify
+            est une entreprise qui crée des sites internet au prix de 10 euros.
+            Le paimenent se fait après la livraison. Visitez leur site internet
+            en cliquant ici https://www.omisify.com
+          </p>
         </div>
       </div>
-      <p>
-        Responsive Design + bonus: Creation de logo, Certificat SSL, Hebergement
-        Creation de site internet au prix de 10 euros. Omisify est une
-        entreprise qui crée des sites internet au prix de 10 euros. Le paimenent
-        se fait après la livraison. Visitez leur site internet en cliquant ici
-        https://www.omisify.com
-      </p>
       <Footer />
     </>
   );
