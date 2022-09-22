6efb3098cac6842291d291604dcbae1f184aca75
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-22 10:49:37 -0200

    m

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <Routes>
         <Route path="*" element={<Home />} />
         <Route path="/" element={<Home />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
