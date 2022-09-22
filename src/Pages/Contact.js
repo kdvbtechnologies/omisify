d3c7de93b83e1cd0ca8b7d288cce0d85ff65775c
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-22 12:02:06 -0200

    m

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,13 @@
 import ContactAction from "../components/ContactAction";
 
 export default function Contact() {
+  const family = {
+    fontFamily:
+      '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  };
   return (
-    <>
+    <div style={family}>
       <ContactAction />
-    </>
+    </div>
   );
 }
