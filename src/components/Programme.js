29ec04dc028aa3ca31963785630ba1224620db48
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-23 19:19:58 -0200

    m

--- src/components/Programme.js
+++ src/components/Programme.js
@@ -4,13 +4,14 @@ export default function Programme() {
       '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
   };
   return (
-    <div style={family}>
-      <h1>Pragramme D'affiliation 1</h1>
+    <div style={family} className="program">
+      <h2>Programme D'affiliation 1</h2>
       <h4>Création de site internet</h4>
       <p>
         Publier un commentaire sur les réseaux sociaux dans lequel vous allez
         insérer ce lien <a href="https://omisify.com">https://omisify.com</a>
       </p>
+
       <p>
         Voilà un exemple de commentaire : Création de site internet au prix de
         10€. Omisify est une entreprise qui crée des sites internet au prix de
@@ -20,7 +21,7 @@ export default function Programme() {
         N.B : Outre les commentaires, vous pouvez aussi publier dans des
         groupes, Envoyer par message à vos proches. Vous pouvez aussi demander à
         vos proches de faire le meme travail que vous sur Omisify cela vaut 2
-        points.
+        points. Maintenant gagnez de l'argent pour chaque commentaire publier.
       </p>
     </div>
   );
