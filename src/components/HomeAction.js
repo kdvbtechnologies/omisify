2d03c569b9c82abc59aca33e59676a18bf47c926
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-22 23:21:29 -0200

    m

--- src/components/HomeAction.js
+++ src/components/HomeAction.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import Footer from "./Footer";
 /*import { Helmet } from "react-helmet";
 <Helmet>
@@ -14,22 +15,111 @@ import Footer from "./Footer";
 export default function HomeAction() {
   return (
     <>
-      <div className="cards">
-        <div className="card">
-          <h3>HTML / CSS</h3>
-          <hr />
+      <div className="display-cards">
+        <div className="cards">
+          <div className="card">
+            <div className="celebrity">
+              <p>Populaire</p>
+            </div>
+            <h3>HTML / CSS</h3>
+            <hr />
+            <div className="card-desc">
+              <p>Création de petit site internet avec HTML5</p>
+              <div className="price">
+                <p>A partir de</p>
+                <h1>10€</h1>
 
-          <div className="card-desc">
-            <p>Creation de petit site internet</p>
-            <p>A partir de 10€</p>
-            <button>Souscrire</button>
-            <p>
-              Responsive Design + bonus: Creation de logo, Certificat SSL,
-              Hebergement Creation de site internet au prix de 10 euros. Omisify
-              est une entreprise qui crée des sites internet au prix de 10
-              euros. Le paimenent se fait après la livraison. Visitez leur site
-              internet en cliquant ici https://www.omisify.com
-            </p>
+                <NavLink to="/contact">
+                  <button>Souscrire</button>
+                </NavLink>
+              </div>
+              <div className="mydesc">
+                <li>Responsive Design</li>
+                <li>paimenent après livraison</li>
+                <p className="bonus">Bonus</p>
+                <li>Création de logo</li>
+                <li>Certificat SSL</li>
+                <li>Hébergement</li>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className="card">
+            <h3>JavaScript</h3>
+            <hr />
+            <div className="card-desc">
+              <p>Création de gros site internet avec JS</p>
+              <div className="price">
+                <p>A partir de</p>
+                <h1>25€</h1>
+                <p>par fonctionnalité</p>
+
+                <NavLink to="/contact">
+                  <button>Souscrire</button>
+                </NavLink>
+              </div>
+              <p>
+                Responsive Design + bonus: Creation de logo, Certificat SSL,
+                Hebergement Creation de site internet au prix de 10 euros.
+                Omisify est une entreprise qui crée des sites internet au prix
+                de 10 euros. Le paimenent se fait après la livraison. Visitez
+                leur site internet en cliquant ici https://www.omisify.com
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className="card">
+            <h3>React JS</h3>
+            <hr />
+            <div className="card-desc">
+              <p>Création de gros site internet avec REACT</p>
+              <div className="price">
+                <p>A partir de</p>
+                <h1>35€</h1>
+                <p>par fonctionnalité</p>
+
+                <NavLink to="/contact">
+                  <button>Souscrire</button>
+                </NavLink>
+              </div>
+              <p>
+                Responsive Design + bonus: Creation de logo, Certificat SSL,
+                Hebergement Creation de site internet au prix de 10 euros.
+                Omisify est une entreprise qui crée des sites internet au prix
+                de 10 euros. Le paimenent se fait après la livraison. Visitez
+                leur site internet en cliquant ici https://www.omisify.com
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className="card">
+            <h3>PHP</h3>
+            <hr />
+            <div className="card-desc">
+              <p>Création de gros site internet avec PHP</p>
+              <div className="price">
+                <p>A partir de</p>
+                <h1>40€</h1>
+                <p>par fonctionnalité</p>
+
+                <NavLink to="/contact">
+                  <button>Souscrire</button>
+                </NavLink>
+              </div>
+              <p>
+                Responsive Design + bonus: Creation de logo, Certificat SSL,
+                Hebergement Creation de site internet au prix de 10 euros.
+                Omisify est une entreprise qui crée des sites internet au prix
+                de 10 euros. Le paimenent se fait après la livraison. Visitez
+                leur site internet en cliquant ici https://www.omisify.com
+              </p>
+            </div>
           </div>
         </div>
       </div>
