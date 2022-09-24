47167f85758838ab43ad5a8e3b128bc3f09f0352
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-24 13:39:29 -0200

    m

---
+++ src/Pages/Partenaire Omisify/DeborahContact.js
@@ -0,0 +1,13 @@
+import DeborahContactAction from "../../components/Partenaire Omisify/DeborahContactAction";
+
+export default function DeborahContact() {
+  const family = {
+    fontFamily:
+      '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  };
+  return (
+    <div style={family}>
+      <DeborahContactAction />
+    </div>
+  );
+}
