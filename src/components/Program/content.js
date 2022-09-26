a972002a2cecd88c31cec0d4e4d3116cb343e006
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-26 15:01:01 -0200

    m

--- src/components/Program/content.js
+++ src/components/Program/content.js
@@ -20,8 +20,8 @@ export const accordionData = [
   {
     title: "Programme 2",
     program: "Programme d'Affiliation : Création de site internet",
-    programLink: "Lien d'Affiliation : https://carilizo.com",
-    programDesc: `Carilizo est une entreprise de création de site internet. Faites créer votre site internet par Carilizo au prix de 10€ seulement. Le payement se fera après la livraison !`,
+    programLink: "Lien d'Affiliation : https://rasivy.com",
+    programDesc: `Rasiy est une entreprise de création de site internet. Faites créer votre site internet par Rasivy au prix de 10€ seulement. Le payement se fera après la livraison !`,
     points: "Nombre de points : 1 point",
     consignes: "Consignes :",
     comment:
