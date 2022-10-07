a7504c3154f05d33cc96e258de48b2ef124058e7
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-07 21:22:23 -0200

    m

--- src/components/ContactPartnerAction.js
+++ src/components/ContactPartnerAction.js
@@ -1,9 +1,13 @@
-import PartnerNavigation from "./Partner-Navigation";
+import { useNavigate } from "react-router-dom";
 
 export default function ContactPartnerAction() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <PartnerNavigation />
+      <div className="back-btn">
+        <button onClick={() => navigate(-1)}>Retour</button>
+      </div>
       <div className="contact">
         <div className="contact-title">
           <h1>Contactez-Nous</h1>
