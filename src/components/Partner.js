c071525ebcc1eaa40fb9af54539f9489b7b4dcc9
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-07 20:46:01 -0200

    m

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -51,7 +51,9 @@ export default function Partner() {
             <h1>Rémunération</h1>
             <li>1 Commentaire = 1 point</li>
             <li>1 Message = 1 point</li>
-            <li>1 Publication = 1 point</li>
+			<li>1 Message Watsapp ou Telegram = 2 points</li>
+			<li>1 E-mail = 2 points</li>
+            <li>1 Publication = 3 points</li>
             <li>1000 Points = 1€</li>
             <li>10 000 Likes = 1€</li>
             <p>
--- src/components/Program/content.js
+++ src/components/Program/content.js
@@ -4,7 +4,7 @@ export const accordionData = [
     program: "Programme d'Affiliation : Devenir Partenaire Omisify",
     programLink: "Lien d'Affiliation : https://omisify.com",
     programDesc: `Omisify est une plateforme qui proprose des programmes d'affiliation. Vous pourrez augmenter vos revenus en devenant Partenaire Omisify. Omisify est gratuit et rémunère toujours ses partenaires quelque soit le pays dans lequel vous vous trouver et quelque soit la situation de votre pays. Avec Omisify vous pourrez retirer votre rémunération à tout moment que vous le souhaiterez. Gagnez de l'argent avec Omisify !`,
-    points: "Nombre de points : 2 points (BONUS DE BIENVENUE)",
+    points: "Bonus CDB : 2 points",
     consignes: "Consignes :",
     comment:
       "Commentaire : Publier un commentaire dans lequel vous insérez votre lien d'affiliation",
@@ -12,18 +12,17 @@ export const accordionData = [
       "Gain : Chaque commentaire publier est équivaut à 1 point et 1 point est équivaut à 0.001€",
     message:
       "Envoyer un message dans lequel vous insérer votre lien d'affiliation",
-    messageG: "Gain : Chaque message envoyé est équivaut à 1 point",
+    messageG: "Gain : Chaque message envoyé est équivaut à 1 point; les messages Whatsapp, Telegram et Signal sont équivaut à 2 points",
     publication:
       "Publier une publication dans lequel vous insérer votre lien d'affiliation",
     publicationG:
-      "Gain : Chaque publication est équivaut à 1 point et chaque like sur une publication est équivaut à 0.0001€",
+      "Gain : Chaque publication est équivaut à 3 points et chaque like sur une publication est équivaut à 0.0001€",
   },
   {
     title: "Programme 2",
     program: "Programme d'Affiliation : Création de site internet",
     programLink: "Lien d'Affiliation : https://rasivy.com",
     programDesc: `Faites créer votre Site internet ou votre Application rapide par Rasivy. Le payement se fera après la livraison !`,
-    points: "Nombre de points : 1 point",
     consignes: "Consignes :",
     comment:
       "Commentaire : Publier un commentaire dans lequel vous insérez votre lien d'affiliation",
@@ -31,11 +30,12 @@ export const accordionData = [
       "Gain : Chaque commentaire publier est équivaut à 1 point et 1 point est équivaut à 0.001€",
     message:
       "Envoyer un message dans lequel vous insérer votre lien d'affiliation",
-    messageG: "Gain : Chaque message envoyé est équivaut à 1 point",
+	messageG: "Gain : Chaque message envoyé est équivaut à 1 point; les messages Whatsapp, Telegram et Signal sont équivaut à 2 points",
+
     publication:
       "Publier une publication dans lequel vous insérer votre lien d'affiliation",
     publicationG:
-      "Gain : Chaque publication est équivaut à 1 point et chaque clic externe sur le lien d'affiliation est équivaut à 0.004€",
+      "Gain : Chaque publication est équivaut à 3 points et chaque clic externe sur le lien d'affiliation est équivaut à 0.004€",
   },
   /*
   {
