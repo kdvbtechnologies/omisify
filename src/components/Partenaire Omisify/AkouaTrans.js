375cd3f201e49d91e7c4eeac84c30d5144eef7c4
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-08 14:29:53 -0200

    m

--- src/components/Partenaire Omisify/AkouaTrans.js
+++ src/components/Partenaire Omisify/AkouaTrans.js
@@ -24,7 +24,7 @@ export default function AkouaTrans() {
           <p>Date de création : 28/09/2022</p>
           <div className="b">
             <p>
-              Dernière mise à jour de la page : 08/10/2022, 14:27 (Heure de
+              Dernière mise à jour de la page : 08/10/2022, 14:29 (Heure de
               Brasilia)
             </p>
           </div>
