f9a65ad57eac132cdae0750453113637f619487d
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-09 22:23:04 -0200

    m

--- src/components/Partenaire Omisify/YetcheHeader.js
+++ src/components/Partenaire Omisify/YetcheHeader.js
@@ -1,16 +1,30 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+//import { useState } from "react";
 import YetcheFooter from "./YetcheFooter";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../feature/posts.slice";
 
 export default function YetcheHeader() {
-  const [posts, setPosts] = useState([]);
+  //const [posts, setPosts] = useState([]);
   const id = "634340e0476255949f2992a3";
-
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.posts);
+  /*
   useEffect(() => {
     axios
       .get(`https://famous-peplum-dove.cyclic.app/api/post/${id}`)
       .then((res) => setPosts(res.data));
-  }, []);
+  }, []);*/
+
+  async function MyPosts() {
+    await axios
+      .get(`https://famous-peplum-dove.cyclic.app/api/post/${id}`)
+      .then((res) => dispatch(setPosts(res.data)))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  MyPosts();
 
   return (
     <>
