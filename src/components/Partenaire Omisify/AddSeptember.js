12c8ca3ac8da79257392893fb15da6111bf5e50c
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-13 21:17:21 -0200

    m

--- src/components/Partenaire Omisify/AddSeptember.js
+++ src/components/Partenaire Omisify/AddSeptember.js
@@ -11,41 +11,34 @@ export default function AddSeptember() {
       .then((res) => {
         console.log(res);
         const idtsept = res.data.idtsept2022;
-        const idtoct = res.data.idtoct;
-        const idtnov = res.data.idtnov;
-        if (idtsept || idtoct || idtnov) {
-          localStorage.setItem("https://omisify.com/idtnov", idtnov);
+
+        if (idtsept) {
           localStorage.setItem("https://omisify.com/idtsept", idtsept);
-          localStorage.setItem("https://omisify.com/idtoct", idtoct);
+        }
+
+        const idtseptt = localStorage.getItem("https://omisify.com/idtsept");
+
+        if (idtseptt) {
+          console.log(idtsept);
+        } else {
+          axios({
+            method: "post",
+            url: "https://famous-peplum-dove.cyclic.app/api/point/add/tseptember",
+            data: {
+              userId,
+              total,
+            },
+          })
+            .then((res) => {
+              console.log(res.data.message);
+              const idtsept = res.data.message;
+              localStorage.setItem("https://omisify.com/idtsept", idtsept);
+            })
+            .catch((err) => console.log(err));
         }
       })
       .catch((err) => console.log(err));
   }, [userId]);
-
-  const idtsept = localStorage.getItem("https://omisify.com/idtsept");
-  const idtoct = localStorage.getItem("https://omisify.com/idtoct");
-  const idtnov = localStorage.getItem("https://omisify.com/idtnov");
-
-  if (idtsept || idtoct || idtnov) {
-    console.log(idtsept);
-    console.log(idtoct);
-    console.log(idtnov);
-  } else {
-    axios({
-      method: "post",
-      url: "https://famous-peplum-dove.cyclic.app/api/point/add/tseptember",
-      data: {
-        userId,
-        total,
-      },
-    })
-      .then((res) => {
-        console.log(res.data.message);
-        const idtsept = res.data.message;
-        localStorage.setItem("https://omisify.com/idtsept", idtsept);
-      })
-      .catch((err) => console.log(err));
-  }
 
   function Next() {
     // update total id
