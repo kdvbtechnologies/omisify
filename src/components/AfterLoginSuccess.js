036bfca8416f38273bab79f52d2516273de99fee
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-13 19:36:47 -0200

    m

--- src/components/AfterLoginSuccess.js
+++ src/components/AfterLoginSuccess.js
@@ -2,6 +2,6 @@ import Auth from "./Auth";
 import Partner from "./Partenaire Omisify/Partner";
 
 export default function AfterLoginSuccess() {
-  const idtotal = localStorage.getItem("https://omisify.com/idtotal");
-  return <>{idtotal ? <Partner /> : <Auth />}</>;
+  const userId = localStorage.getItem("https://omisify.com/idtotal");
+  return <>{userId ? <Partner /> : <Auth />}</>;
 }
