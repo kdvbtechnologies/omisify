3a33ebc6071c206212445cf4812f8f2c66eeb66d
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-13 10:28:26 -0200

    m

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -3,13 +3,11 @@ import { NavLink } from "react-router-dom";
 export default function Connexion() {
   return (
     <>
-      <div
-        className="connexion"
-        style={{ textAlign: "center", paddingTop: "200px" }}
-      >
+      <div className="connexion">
         <p>Portail des Partenaires Omisify</p>
         <h1>Connexion</h1>
         <input type="text" placeholder="E-mail" />
+        <input type="text" placeholder="Mot de passe" />
         <NavLink to="/partner">
           <button>Se connecter</button>
         </NavLink>
