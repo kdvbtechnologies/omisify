7f7e7c8c4cfe24933047d975fed6953a3142cefc
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-14 17:20:15 -0200

    m.

--- src/App.js
+++ src/App.js
@@ -66,7 +66,6 @@ import PartnerSettings from "./components/Partenaire Omisify/PartnerSettings";
 import Auth from "./components/Auth";
 import AfterLogin from "./components/AfterLogin";
 import AfterLoginSuccess from "./components/AfterLoginSuccess";
-import LoginSuccess from "./components/LoginSuccess";
 
 export default function App() {
   return (
@@ -134,7 +133,6 @@ export default function App() {
         <Route path="/auth" element={<Auth />} />
         <Route path="/after-login" element={<AfterLogin />} />
         <Route path="/partner" element={<AfterLoginSuccess />} />
-        <Route path="/login-success" element={<LoginSuccess />} />
 
         <Route path="/simo" element={<SimoRandy />} />
         <Route path="/simo-menu" element={<SimoNavMenu />} />
