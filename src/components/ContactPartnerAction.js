226324663405867040464c313c4399bb46439036
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-14 20:54:49 -0200

    m.

--- src/components/ContactPartnerAction.js
+++ src/components/ContactPartnerAction.js
@@ -10,7 +10,7 @@ export default function ContactPartnerAction() {
       </div>
       <div className="contact">
         <div className="contact-title">
-          <h1>Service partenaire</h1>
+          <h3>Service partenaire</h3>
           <p>E-mail : contact@omisify.com</p>
           <p>Whatsapp : +5514991550021</p>
         </div>
