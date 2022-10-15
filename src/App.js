21134bfbcd0addc7aae254f71675957d58e5907e
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-15 16:09:45 -0200

    m.

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ import SecureMWI from "./components/September/SecureMWI";
 import SecurePublication from "./components/September/SecurePublication";
 import SecureEmail from "./components/September/SecureEmail";
 import SecurePWI from "./components/September/SecurePWI";
+import UpdateSeptember from "./components/Partenaire Omisify/UpdateSeptember";
 
 export default function App() {
   return (
@@ -152,6 +153,7 @@ export default function App() {
         <Route path="/secure-mwi" element={<SecureMWI />} />
         <Route path="/secure-publication" element={<SecurePublication />} />
         <Route path="/secure-pwi" element={<SecurePWI />} />
+        <Route path="/update-sept" element={<UpdateSeptember />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Partenaire Omisify/UpdateSeptember.js
+++ src/components/Partenaire Omisify/UpdateSeptember.js
@@ -1,36 +1,48 @@
 import { NavLink } from "react-router-dom";
+import PartnerNavigation from "../Partner-Navigation";
 
 export default function UpdateSeptember() {
+  const family = {
+    fontFamily:
+      '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  };
+
   return (
-    <>
-      <h1>Choisissez :</h1>
-      <NavLink to="/secure-comment">
-        <p>Commentaire</p>
-      </NavLink>
-      <NavLink to="/secure-email">
-        <p>Email</p>
-      </NavLink>
-      <NavLink to="/secure-like-comment">
-        <p>Like (commentaire)</p>
-      </NavLink>
-      <NavLink to="/secure-like-publication">
-        <p>Like (publication)</p>
-      </NavLink>
-      <NavLink to="/secure-m10k">
-        <p>M10k</p>
-      </NavLink>
-      <NavLink to="/secure-message">
-        <p>Message</p>
-      </NavLink>
-      <NavLink to="/secure-mwi">
-        <p>MWI</p>
-      </NavLink>
-      <NavLink to="/secure-publication">
-        <p>Publication</p>
-      </NavLink>
-      <NavLink to="/secure-pwi">
-        <p>PWI</p>
-      </NavLink>
-    </>
+    <div style={family}>
+      <PartnerNavigation />
+      <div className="update">
+        <div className="update-title">
+          <p>Actualisez vos points</p>
+        </div>
+        <p>Choisissez et cliquez sur ce que vous voulez actualiser :</p>
+        <NavLink className="navlink" to="/secure-comment">
+          <p>Commentaire</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-email">
+          <p>Email</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-like-comment">
+          <p>Like (commentaire)</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-like-publication">
+          <p>Like (publication)</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-m10k">
+          <p>M10k</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-message">
+          <p>Message</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-mwi">
+          <p>MWI</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-publication">
+          <p>Publication</p>
+        </NavLink>
+        <NavLink className="navlink" to="/secure-pwi">
+          <p>PWI</p>
+        </NavLink>
+      </div>
+    </div>
   );
 }
