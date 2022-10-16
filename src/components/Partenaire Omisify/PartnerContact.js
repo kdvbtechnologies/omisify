495e649ed952a839d2d638a3b1814a5a7a746cd0
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-16 16:04:12 -0200

    m.

--- src/components/Partenaire Omisify/PartnerContact.js
+++ src/components/Partenaire Omisify/PartnerContact.js
@@ -13,6 +13,7 @@ export default function PartnerContact() {
           <h3>Service partenaire</h3>
           <p>E-mail : contact@omisify.com</p>
           <p>Whatsapp : +5514991550021</p>
+          <p>Telegram : +5514991550021</p>
         </div>
       </div>
     </>
