f1ed6657376229b737e7be5f5bab511ea6d6b264
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-16 17:59:18 -0200

    m.

--- src/components/Admin/Panel.js
+++ src/components/Admin/Panel.js
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function Panel() {
+  useEffect(() => {
+    async function get() {
+      await axios
+        .get("https://famous-peplum-dove.cyclic.app/api/panel/")
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+    get();
+  }, []);
   return (
     <>
       <p>Panneau d'administration</p>
