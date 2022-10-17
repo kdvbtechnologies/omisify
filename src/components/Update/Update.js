b526d1b65d1b337e38a456a854d5fb4225e9cf74
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-17 05:37:04 -0200

    m.

--- src/components/Update/Update.js
+++ src/components/Update/Update.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import LoginNav from "../Auth/LoginNav";
+import Loader from "../Partenaire Omisify/Loader";
 
 export default function Update() {
+  const [isLoading, setIsLoading] = useState(false);
+
   async function Download() {
+    setIsLoading(true);
     window.location.reload();
   }
 
   return (
     <>
       <LoginNav />
-      <p>Obtenir la dernière version d'Omisify</p>
-      <button onClick={Download}>Télécharger</button>
+      <p>Mise à jour</p>
+      <p>Obtenir la dernière mise à jour</p>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <button onClick={Download}>Télécharger</button>
+        </>
+      )}
     </>
   );
 }
