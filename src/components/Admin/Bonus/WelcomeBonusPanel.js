42ba628d04aae6f29aa8dabb721a09783b60e765
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-18 10:53:28 -0200

    m

--- src/components/Admin/Bonus/WelcomeBonusPanel.js
+++ src/components/Admin/Bonus/WelcomeBonusPanel.js
@@ -45,9 +45,9 @@ export default function WelcomeBonusPanel() {
     <div style={family}>
       <LoginNav />
       <PanelNav />
+      <h3>Code de Bienvenue</h3>
       {api.map((api) => (
         <>
-          <h4>Code de Bienvenue</h4>
           <p>Noms du demandeur : {api.partnername}</p>
           <p>Noms court du demandeur : {api.shortname}</p>
           <p>Code de Bienvenue du mentor : {api.codewelcomementor}</p>
