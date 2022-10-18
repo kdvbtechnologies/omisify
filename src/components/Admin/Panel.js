1f07e273d35fa83f2aba109c4a612f4817a962c8
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-18 08:47:52 -0200

    m..

--- src/components/Admin/Panel.js
+++ src/components/Admin/Panel.js
@@ -17,11 +17,11 @@ export default function Panel() {
     }
     get();
   }, []);
-
+  /*
   const welcomebonus = 100;
   const userId = `${api.userId}`;
-  const codewelcomementor = `${api.codewelcomementor}`;
-
+  const codewelcomementor = `${api.codewelcomementor}`;*/
+  /*
   async function Validate() {
     await axios({
       method: "put",
@@ -33,29 +33,20 @@ export default function Panel() {
     })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-  }
+  }*/
 
   return (
     <>
       <LoginNav />
       {api.map((api) => (
         <>
-          <p>Panneau d'administration</p>
-          <p>User :</p>
-          <p>Nombre de commentaires : {api.commentrecent}</p>
-          <p>Nombre total de commentaires : {api.numbertcommentlife}</p>
-          <p>Point total depuis le début : {api.pointtlife}</p>
-          <p>Gain : {api.gaintlife}</p>
-        </>
-      ))}
-      {api.map((api) => (
-        <>
-          <p>Code de Bienvenue</p>
+          <h1>Code de Bienvenue</h1>
           <p>Noms du demandeur : {api.partnername}</p>
           <p>Noms court du demandeur : {api.shortname}</p>
           <p>Code de Bienvenue du mentor : {api.codewelcomementor}</p>
-          <p>Bonus de Bienvenue à ajouter : {welcomebonus} points</p>
-          <button onClick={Validate}>Valider</button>
+          <p>- - -- - -- - -- - -- - -- - -- - -</p>
+          <p>Bonus de Bienvenue recu : {api.welcomebonus} point(s)</p>
+          <p>________________________________</p>
         </>
       ))}
     </>
