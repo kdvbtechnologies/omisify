73b1fc7b1760336d7d3ad1759848cef836ff1761
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-18 14:27:28 -0200

    m

--- src/components/Admin/User/User.js
+++ src/components/Admin/User/User.js
@@ -25,17 +25,24 @@ export default function User() {
     <div style={family}>
       <LoginNav />
       <PanelNav />
-      <div className="panel">
-        <h3>Listes des Partenaires et leurs infos</h3>
-        {api.map((api) => (
-          <>
-            <p>{api.name}</p>
-            <p>{api.partnername}</p>
-            <p>{api.shortname}</p>
-            <hr />
-            <hr />
-          </>
-        ))}
+      <div className="panel-main">
+        <div className="panel">
+          <h3>Listes des Partenaires et leurs infos</h3>
+          <p>cava</p>
+          <p>bonjour</p>
+          <p>salut</p>
+          <hr />
+          <hr />
+          {api.map((api) => (
+            <>
+              <p>{api.name}</p>
+              <p>{api.partnername}</p>
+              <p>{api.shortname}</p>
+              <hr />
+              <hr />
+            </>
+          ))}
+        </div>
       </div>
     </div>
   );
