3e8bcbeb526a729fbc1707b6200d9e8789c36d4c
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-19 19:56:08 -0200

    m

--- src/components/Bonus/DaysOfGenerosity/DaysOfGenerosity.js
+++ src/components/Bonus/DaysOfGenerosity/DaysOfGenerosity.js
@@ -1,8 +1,27 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Loader from "../../Partenaire Omisify/Loader";
 
 export default function DaysOfGenerosity() {
-  //const userId = localStorage.getItem("https://omisify.com/userId");
+  const userId = localStorage.getItem("https://omisify.com/userId");
   const navigate = useNavigate();
+  const [generositydays, setGenerosityDays] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function Save() {
+    setIsLoading(true);
+    await axios({
+      method: "put",
+      url: `${process.env.REACT_APP_OMISIFY_API}/api/user/updateuserinfos/${userId}`,
+      data: {
+        generositydays,
+      },
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    //window.location = "/daysofgenerosity-success";
+  }
 
   const family = {
     fontFamily:
@@ -25,10 +44,28 @@ export default function DaysOfGenerosity() {
             jours dans la semaine. Les points que le Partenaire gagnent pendant
             les journées de générosité sont multipliés par 10
           </p>
-          <p>Entrer vos jours de générosité</p>
-          <input type="text" placeholder="Jours de générosité" />
+          <p>Choisissez vos journées de générosité</p>
+          <select
+            id="generosity-days"
+            value={generositydays}
+            onChange={(e) => setGenerosityDays(e.target.value)}
+          >
+            <option value="Lundi et Jeudi">Lundi et Jeudi</option>
+            <option value="Mardi et Vendredi">Mardi et Vendredi</option>
+            <option value="Mercredi et Samedi">Mercredi et Samedi</option>
+            <option value="Jeudi et Dimanche">Jeudi et Dimanche</option>
+            <option value="Vendredi et Lundi">Vendredi et Lundi</option>
+            <option value="Samedi et Mardi">Samedi et Mardi</option>
+            <option value="Dimanche et Mercredi">Dimanche et Mercredi</option>
+          </select>
         </div>
-        <button>Sauvegarder</button>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <button onClick={Save}>Sauvegarder</button>
+          </>
+        )}
       </div>
     </div>
   );
