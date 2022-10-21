4658de3fb1101702a3bfb0d72a3b4ba2656f2796
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-21 05:47:07 -0200

    m

--- src/components/UpdatePoint/October/Email/InitializeNewPointEmail.js
+++ src/components/UpdatePoint/October/Email/InitializeNewPointEmail.js
@@ -107,7 +107,7 @@ export default function InitializeNewPointEmail() {
         );
       })
       .catch((err) => console.log(err));
-    window.location.reload();
+    //window.location.reload();
   }
 
   const family = {
