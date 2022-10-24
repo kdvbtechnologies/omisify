fef641cedf7d6d1a2d6f9783ff7148c65fc8cfd0
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-24 20:07:03 -0200

    m

--- src/components/Admin/UpdatePoint/TotalPointPanel.js
+++ src/components/Admin/UpdatePoint/TotalPointPanel.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { useState } from "react";
-import LoginNav from "../../Auth/LoginNav";
-import PanelNav from "../Navigation/PanelNav";
 
 export default function TotalPointPanel() {
   const [userId, setUserId] = useState("");
