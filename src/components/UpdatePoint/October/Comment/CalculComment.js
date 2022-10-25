1096d3691a9cbfc026a1deaf0b4dc1e8accae229
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-25 08:42:57 -0200

    m

--- src/components/UpdatePoint/October/Comment/CalculComment.js
+++ src/components/UpdatePoint/October/Comment/CalculComment.js
@@ -2,21 +2,32 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function CalculComment() {
-  const [newnumber, setNewnumber] = useState("");
-  const oldtotal = 10;
-  const result = parseInt(newnumber) + parseInt(oldtotal);
-
   useEffect(() => {
     async function get() {
       await axios
         .get(
           "https://famous-peplum-dove.cyclic.app/api/user/634c8ab0f59d4e9994f83884"
         )
-        .then((res) => console.log(res))
+        .then((res) => {
+          console.log(res);
+          const pointtcommentlife = res.data.pointtcommentlife;
+          if (pointtcommentlife) {
+            localStorage.setItem(
+              "https://omisify.com/pointtcommentlife",
+              pointtcommentlife
+            );
+          }
+        })
         .catch((err) => console.log(err));
     }
     get();
   }, []);
+
+  const [newnumber, setNewnumber] = useState("");
+  const pointtcommentlife = localStorage.getItem(
+    "https://omisify.com/pointtcommentlife"
+  );
+  const result = parseInt(newnumber) + parseInt(pointtcommentlife);
 
   function Save() {
     localStorage.setItem("https://omisify.com/resultsave", result);
@@ -24,10 +35,10 @@ export default function CalculComment() {
   }
   return (
     <>
-      <p>Ancien total des commentaires : {oldtotal}</p>
+      <p>Ancien total des commentaires : {pointtcommentlife}</p>
       <p>Opération</p>
       <p>
-        {newnumber} + {oldtotal} = {result}
+        {newnumber} + {pointtcommentlife} = {result}
       </p>
       <p>Nouveau nombre</p>
       <input
