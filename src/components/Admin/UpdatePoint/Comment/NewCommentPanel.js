dc67cde79d1f60cb6e0f7193d64bdd2b044705f1
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-25 15:58:19 -0200

    m

--- src/components/Admin/UpdatePoint/Comment/NewCommentPanel.js
+++ src/components/Admin/UpdatePoint/Comment/NewCommentPanel.js
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+import Loader from "../../../Partenaire Omisify/Loader";
+
 export default function NewCommentPanel() {
+  const [userId, setUserId] = useState("");
+  const [numbercommententeradmin, setNumbercommententeradmin] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function Validate(e) {
+    setIsLoading(true);
+    e.preventDefault();
+    await axios({
+      method: "put",
+      url: `${process.env.REACT_APP_OMISIFY_API}/api/user/updatecomment/${userId}`,
+      data: {
+        numbercommententeradmin,
+      },
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    setIsLoading(false);
+  }
   const family = {
     fontFamily:
       '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
@@ -14,9 +36,32 @@ export default function NewCommentPanel() {
           <p>Commentaire</p>
         </div>
         <div className="align">
+          <div className="a">
+            <p>Entrer le userId</p>
+            <input
+              type="text"
+              placeholder="userId"
+              value={userId}
+              onChange={(e) => setUserId(e.target.value)}
+              name="userId"
+            />
+          </div>
+
           <p>Entrer le nombre de nouveau(x) commentaire(s)</p>
-          <input type="number" placeholder="Nombre de commentaire(s)" />
-          <button>Valider</button>
+          <input
+            type="number"
+            placeholder="Nombre de commentaire(s)"
+            value={numbercommententeradmin}
+            onChange={(e) => setNumbercommententeradmin(e.target.value)}
+            name="numbercommententeradmin"
+          />
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <button onClick={Validate}>Valider</button>
+            </>
+          )}
         </div>
       </div>
     </>
