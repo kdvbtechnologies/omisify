8ac0e7fa8559a91143edafd6f07cd6c065715e6d
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-26 17:09:48 -0200

    m

--- src/components/Navigation/SecureNavigation.js
+++ src/components/Navigation/SecureNavigation.js
@@ -1,8 +1,8 @@
-import Auth from "../Auth";
+import AuthSignup from "../AuthSignup";
 import PartnerNavMenu from "../Partenaire Omisify/PartnerNavMenu";
 
 export default function SecureNavigation() {
   const userId = localStorage.getItem("https://omisify.com/userId");
   const shortname = localStorage.getItem("https://omisify.com/shortname");
-  return <>{userId && shortname ? <PartnerNavMenu /> : <Auth />}</>;
+  return <>{userId && shortname ? <PartnerNavMenu /> : <AuthSignup />}</>;
 }
