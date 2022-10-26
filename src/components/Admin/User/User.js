f67c6bbacbf526610b4f69b59812ebfd6af7b280
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-26 15:48:46 -0200

    m

--- src/components/Admin/User/User.js
+++ src/components/Admin/User/User.js
@@ -45,26 +45,26 @@ export default function User() {
                   <p>Noms de naissance : {api.name}</p>
                   <p>Noms principal : {api.partnername}</p>
                   <p>Noms court : {api.shortname}</p>
-                  <p>Code de Bienvenue : {api.codewelcome}</p>
-                  <p>Bonus de Bienvenue recu : {api.welcomebonus} point(s)</p>
-                  <p>Journées de générosité : {api.generositydays}</p>
+                  <p>Code CB : {api.codewelcome}</p>
+                  <p>Bonus B : {api.welcomebonus} point(s)</p>
+                  <p>Bonus EP : {api.bestpartnerbonus}</p>
+                  <p>Bonus JG : {api.generositydaysbonus} point(s)</p>
+                  <p>Journées de générosité : {api.generositydays} point(s)</p>
                   <p>Méthode de Paiement préféré : {api.paymentmethod}</p>
                   <p>...</p>
                   <p>Commentaires : {api.pointtcommentlife} point(s)</p>
                   <p>E10k : {api.pointte10klife} point(s)</p>
-                  <p>
-                    Likes (commentaire) :{api.pointtlikescommentlife} point(s)
-                  </p>
-                  <p>
-                    Likes (publication) : {api.pointtlikespublicationlife}{" "}
-                    point(s)
-                  </p>
-                  <p>Publications : {api.pointtpublicationlife} point(s)</p>
-                  <p>PWI : {api.pointtpwilife} point(s)</p>
+                  <p>E-mail : {api.pointtemaillife} point(s)</p>
+                  <p>Likes C :{api.pointtlikescommentlife} point(s)</p>
+                  <p>Likes P : {api.pointtlikespublicationlife} point(s)</p>
+
                   <p>M10k : {api.pointtm10klife} point(s)</p>
                   <p>Messages : {api.pointtmessagelife} point(s)</p>
                   <p>MWI : {api.pointtmwilife} point(s)</p>
-                  <p>E-mail : {api.pointtemaillife} point(s)</p>
+
+                  <p>Publications : {api.pointtpublicationlife} point(s)</p>
+                  <p>PWI : {api.pointtpwilife} point(s)</p>
+
                   <p>Total des Points : {api.pointtlife} point(s)</p>
                   <p>Gains : {api.gaintlife}€</p>
                   <p>---</p>
