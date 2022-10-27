969510875ff49201be35b5aeafe71ee4a7baf2de
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-27 04:40:34 -0200

    m

--- src/components/UpdatePoint/October/Comment/CalculComment.js
+++ src/components/UpdatePoint/October/Comment/CalculComment.js
@@ -105,7 +105,7 @@ export default function CalculComment() {
   on additionne le tout par le gaintlife qu'on avait recuperer dans le localStorage 
   */
 
-  const calculgaintlife = parseInt(oldgaintlife) + parseInt(newnumber) * 0.001;
+  const calculgaintlife = parseInt(oldgaintlife) + +parseInt(newnumber) * 0.001;
   console.log(calculgaintlife);
 
   const calculpointtcommentlife =
