4bb1280abdb5f724ab8e354b12656e8e4741c665
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-28 20:05:34 -0200

    m

--- src/components/Admin/Bonus/WelcomeBonusPanel.js
+++ src/components/Admin/Bonus/WelcomeBonusPanel.js
@@ -26,6 +26,13 @@ export default function WelcomeBonusPanel() {
     }
     get();
   }, []);
+  /*
+         src={api.filter((u) => u.id === api?.userId)[5].userId}
+
+                        .sort((a, b) => b._id - a._id)
+
+
+  */
 
   const family = {
     fontFamily:
@@ -41,37 +48,34 @@ export default function WelcomeBonusPanel() {
         <div className="title">
           <p>Demande du Bonus de Bienvenue</p>
         </div>
-        {api.filter((u) => u.id === api?.userId)[5].userId}
         {isLoading ? (
           <Loader />
         ) : (
           <>
-            {api
-              .sort((a, b) => b._id - a._id)
-              .map((api) => (
-                <>
-                  <div className="shrink">
-                    <div className="a">
-                      <div className="name">
-                        <p>{api.partnername}</p>
-                      </div>
-
-                      <p>Noms : {api.name}</p>
-                      <p>Code BM : {api.codewelcomementor}</p>
-                      <p>Bonus EP : {api.bestpartnerbonus}</p>
-
-                      <div className="b">
-                        <p>Demande d'Affiches</p>
-                        <p>PA : {api.askposter}</p>
-                      </div>
-
-                      <p>Créé : {dateParser(api.createdAt)}</p>
-                      <p>Modifié : {dateParser(api.updatedAt)}</p>
-                      <p>userId : {api.userId}</p>
+            {api.map((api) => (
+              <>
+                <div className="shrink">
+                  <div className="a">
+                    <div className="name">
+                      <p>{api.partnername}</p>
                     </div>
+
+                    <p>Noms : {api.name}</p>
+                    <p>Code BM : {api.codewelcomementor}</p>
+                    <p>Bonus EP : {api.bestpartnerbonus}</p>
+
+                    <div className="b">
+                      <p>Demande d'Affiches</p>
+                      <p>PA : {api.askposter}</p>
+                    </div>
+
+                    <p>Créé : {dateParser(api.createdAt)}</p>
+                    <p>Modifié : {dateParser(api.updatedAt)}</p>
+                    <p>userId : {api.userId}</p>
                   </div>
-                </>
-              ))}
+                </div>
+              </>
+            ))}
           </>
         )}
       </div>
