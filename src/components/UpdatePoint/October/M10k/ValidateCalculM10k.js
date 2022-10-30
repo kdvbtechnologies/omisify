f01f3ebe80489794588a411b36f378a117153611
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-30 19:02:24 -0200

    m

--- src/components/UpdatePoint/October/M10k/ValidateCalculM10k.js
+++ src/components/UpdatePoint/October/M10k/ValidateCalculM10k.js
@@ -2,18 +2,17 @@ import axios from "axios";
 
 export default function ValidateCalculM10k() {
   const pointtm10klife = localStorage.getItem(
-    "https://omisify.com/pointtm10klife"
+    "https://omisify.com/resultpointtm10klife"
   );
 
   async function Validate() {
-    await axios
-      .put({
-        method: "put",
-        url: `${process.env.REACT_APP_OMISIFY_API}/api/user/updatem10k/634c8ab0f59d4e9994f83884`,
-        data: {
-          pointtm10klife,
-        },
-      })
+    await axios({
+      method: "put",
+      url: `${process.env.REACT_APP_OMISIFY_API}/api/user/updatem10k/634c8ab0f59d4e9994f83884`,
+      data: {
+        pointtm10klife,
+      },
+    })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }
