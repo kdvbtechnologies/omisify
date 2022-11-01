c713db35927572c9ec62130e7dcfc913f1b41acb
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-01 10:02:04 -0200

    m

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export default function Test() {
   async function Get() {
     await axios
-      .get("https://geolocation-db.com/")
+      .get(
+        "https://geolocation-db.com/json/c5544c60-581f-11ed-b302-a99a705cf413/"
+      )
       .then((res) => {
         console.log(res);
       })
