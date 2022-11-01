c43033934a04c33d79bc44640fa78ee050adf3c5
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-01 10:32:14 -0200

    m

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,18 +1,31 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Test() {
-  async function Get() {
-    await axios
-      .get("https://ip.nf/me.json")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
-  }
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    async function get() {
+      await axios
+        .get("https://ip.nf/me.json")
+        .then((res) => {
+          console.log(res.data.ip);
+          setApi(res.data.ip);
+        })
+        .catch((err) => console.log(err));
+    }
+    get();
+  }, []);
 
   return (
     <>
-      <button onClick={Get}>Get Ip</button>
+      {api.map((api) => (
+        <>
+          <p>{api.country}</p>
+          <p>{api.city}</p>
+          <p>{api.ip}</p>
+        </>
+      ))}
     </>
   );
 }
