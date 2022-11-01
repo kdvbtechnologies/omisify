53a7c40780635003a7a38917c367685bf8edb516
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-01 17:21:49 -0200

    m

--- src/components/Home/A.js
+++ src/components/Home/A.js
@@ -13,20 +13,58 @@ export default function A() {
       .then((res) => {
         console.log(res.data.ip.country);
 
-        const countryip = res.data.ip.country;
-        if (countryip) {
-          localStorage.setItem("https://omisify.com/countryip", countryip);
+        const getcountryip = res.data.ip.country;
+        if (getcountryip) {
+          localStorage.setItem("https://omisify.com/countryip", getcountryip);
         }
 
-        const cityip = res.data.ip.city;
-        if (cityip) {
-          localStorage.setItem("https://omisify.com/cityip", cityip);
+        const getcityip = res.data.ip.city;
+        if (getcityip) {
+          localStorage.setItem("https://omisify.com/cityip", getcityip);
         }
 
-        const userip = res.data.ip.ip;
-        if (userip) {
-          localStorage.setItem("https://omisify.com/userip", userip);
+        const getuserip = res.data.ip.ip;
+        if (getuserip) {
+          localStorage.setItem("https://omisify.com/userip", getuserip);
         }
+
+        const page = "Page d'accueil Omisify visité";
+        const countryip = localStorage.getItem("https://omisify.com/countryip");
+        const cityip = localStorage.getItem("https://omisify.com/cityip");
+        const userip = localStorage.getItem("https://omisify.com/userip");
+        const userId = localStorage.getItem("https://omisify.com/userId");
+        const name = localStorage.getItem("https://omisify.com/name");
+        const shortname = localStorage.getItem("https://omisify.com/shortname");
+        const partnername = localStorage.getItem(
+          "https://omisify.com/partnername"
+        );
+
+        const getdate = new Date();
+        const date = getdate.toLocaleDateString("fr-FR", {
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+        });
+
+        axios({
+          method: "post",
+          url: `${process.env.REACT_APP_OMISIFY_API}/api/panel/add`,
+          data: {
+            countryip,
+            cityip,
+            userip,
+            page,
+            date,
+            userId,
+            shortname,
+            partnername,
+            name,
+          },
+        })
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
       })
       .catch((err) => console.log(err));
   }
