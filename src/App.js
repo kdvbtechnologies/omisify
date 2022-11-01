477fb7a64a90a96b2d1fa85c75764deedf532f21
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-01 20:31:35 -0200

    m

--- src/App.js
+++ src/App.js
@@ -234,12 +234,14 @@ import AfterAccessCode from "./components/Others/AccessCode/AfterAccessCode";
 import SecureAccessCode from "./components/Others/AccessCode/SecureAccessCode";
 import Prices from "./components/Business/Home/Prices/Prices";
 import Test from "./components/Test";
+import Translation from "./components/Translation/Translation";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/test" element={<Test />} />
+        <Route path="/translation" element={<Translation />} />
         <Route path="/validate-access-code" element={<ValidateAccessCode />} />
         <Route path="/error-access-code" element={<ErrorAccessCode />} />
         <Route path="/after-access-code" element={<AfterAccessCode />} />
---
+++ src/components/Translation/Translation.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+export default function Translation() {
+  const { t, i18n } = useTranslation();
+  const [languageModel, setLanguageModel] = useState(
+    localStorage.getItem("language-model") === "true"
+  );
+
+  // language model (change language and select language)
+  // Here change language
+  function ChangeLanguage(lng) {
+    i18n.changeLanguage(lng);
+    setLanguageModel(lng);
+  }
+
+  const storage = localStorage.getItem("i18nextLng");
+
+  const en = "en";
+  const preferenceLangEn = storage === en;
+
+  const pt = "pt";
+  const preferenceLangPt = storage === pt;
+
+  const es = "es";
+  const preferenceLangEs = storage === es;
+
+  const fr = "fr";
+  const preferenceLangFr = storage === fr;
+
+  // toggle language model + localStorage
+  useEffect(() => {
+    localStorage.setItem("language-model", languageModel);
+  }, [languageModel]);
+
+  const family = {
+    fontFamily:
+      ' "Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif ',
+  };
+
+  return (
+    <div style={family}>
+      <div className="translation">
+        <div className="title">
+          <p>{t("--choose-language")}</p>
+        </div>
+
+        <div className="button">
+          <button
+            className={`${preferenceLangEn ? "btn-active" : "btn"}`}
+            onClick={() => ChangeLanguage("en")}
+          >
+            English
+          </button>
+        </div>
+
+        <div className="button">
+          <button
+            className={`${preferenceLangPt ? "btn-active" : "btn"}`}
+            onClick={() => ChangeLanguage("pt")}
+          >
+            Português
+          </button>
+        </div>
+
+        <div className="button">
+          <button
+            className={`${preferenceLangEs ? "btn-active" : "btn"}`}
+            onClick={() => ChangeLanguage("es")}
+          >
+            Español
+          </button>
+        </div>
+
+        <div className="button">
+          <button
+            className={`${preferenceLangFr ? "btn-active" : "btn"}`}
+            onClick={() => ChangeLanguage("fr")}
+          >
+            Français
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
