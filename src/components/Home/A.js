2a851939ab057bf9023ad5304fbb731e4810454b
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-01 16:58:54 -0200

    m

--- src/components/Home/A.js
+++ src/components/Home/A.js
@@ -1,10 +1,36 @@
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 export default function A() {
   const userId = localStorage.getItem("https://omisify.com/userId");
   const partnername = localStorage.getItem("https://omisify.com/partnername");
   const shortname = localStorage.getItem("https://omisify.com/shortname");
   const name = localStorage.getItem("https://omisify.com/name");
+
+  async function get() {
+    await axios
+      .get("https://ip.nf/me.json")
+      .then((res) => {
+        console.log(res.data.ip.country);
+
+        const countryip = res.data.ip.country;
+        if (countryip) {
+          localStorage.setItem("https://omisify.com/countryip", countryip);
+        }
+
+        const cityip = res.data.ip.city;
+        if (cityip) {
+          localStorage.setItem("https://omisify.com/cityip", cityip);
+        }
+
+        const userip = res.data.ip.ip;
+        if (userip) {
+          localStorage.setItem("https://omisify.com/userip", userip);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+  get();
 
   return (
     <div className="a">
