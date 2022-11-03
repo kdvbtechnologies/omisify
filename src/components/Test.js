2ee6382c3ddf29bc71f7eeda1a8c9a3acf4a112f
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-03 07:00:22 -0200

    m

--- src/components/Test.js
+++ src/components/Test.js
@@ -4,9 +4,18 @@ export default function Test() {
     window.location = "https://jamelfase.com";
   }
 
+  async function Print(e) {
+    e.preventDefault();
+    window.print();
+  }
+
   return (
     <>
       <button onClick={Download}>Download</button>
+      <br />
+      <button align="center" onClick={Print}>
+        Imprimer
+      </button>
     </>
   );
 }
