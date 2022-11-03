d7d6bad79c914fc2634f9d76c42e19726857f899
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-03 07:40:07 -0200

    m

--- src/components/Test.js
+++ src/components/Test.js
@@ -20,6 +20,25 @@ export default function Test() {
   }
   console.log(GetLanguage);
 
+  function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    const time = h + ":" + m + ":" + s;
+    console.log(time);
+    setTimeout(startTime, 1000);
+  }
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    } // add zero in front of numbers < 10
+    return i;
+  }
+
   return (
     <>
       <button onClick={Download}>Download</button>
@@ -28,7 +47,9 @@ export default function Test() {
       <br />
       <button onClick={Back}>Retour</button>
       <p id="demo"></p>
-      <button onClick={{ GetLanguage }}>Detect Language</button>
+      <button onClick={GetLanguage}>Detect Language</button>
+      <br />
+      <button onClick={startTime}>Obtenir heure</button>
     </>
   );
 }
